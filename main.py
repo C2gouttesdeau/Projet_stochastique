d27f7433475f4c80e718916d9853a8a6941ccc4f
C2gouttesdeau <tanguy.roussin@gmail.com>
2023-12-06 16:02:45 +0100

    dev_tanguy

--- main.py
+++ main.py
@@ -8,73 +8,104 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.animation as animation
 import matplotlib.image as mpimg
+import time
+import cmath
+
 
 ##### Import files #####
 from routines import *
 
-##### To change #####
-l=10            #longueur de la boite
-a=1             #echelle de longueur
-N=50           #nombre de particules
-v0 = 0.03      #Vitesse
-dt = 1          #Pas de temps
-eta = 0         #eta
-Nt = 1       #Nombre d'itérations
-T = Nt*dt       #Temps final
-Simulation_name = ""
-######################
-##### Initialisation #####
+################################### To change ###########################################
+l=10                   #longueur de la boite                                             #
+a=1                    #echelle de longueur                                              #
+N=200                  #nombre de particules                                             #
+v0 = 0.03              #Vitesse                                                          #
+dt = 1                 #Pas de temps                                                     #
+eta = 0.1              #eta : paramètre d'aléatoire                                      #
+Nt = 10                #Nombre d'itérations                                              #
+T = Nt*dt              #Temps final                                                      #
+Simulation_name = "test"  #Nom de la simulation                                          #
+Show_init = False      #True pour afficher les positions initiales dans un graph         #
+Animation = True       #True pour afficher l'animation                                   #
+Save = False           #True pour sauvegarder l'animation                                #
+Trajectoires = False   #True pour afficher les trajectoires (gérer pour N=1)             #
+Background = False     #True pour afficher le fond                                       #
+Animation_infini =False#True pour afficher l'animation avec un nombre infini d'itérations#
+#########################################################################################
 
-#Initialisation des positions et directions
+
+##### Initialisation #####
 x_init, y_init, theta_init = position_direction_init(N,l)
 #Affichage des positions initiales
-Show = False
-if Show==True:
+if Show_init==True:
     plt.figure(1)
     plt.scatter(x_init,y_init)
     plt.xlim(0,l)
     plt.ylim(0,l)
     plt.show()
 
-#Histogramme
-# plt.figure(1)
-# plt.hist(x_init,y_init,width = 5, edgecolor = 'white')
-# plt.show()
-
-x_sol , y_sol, theta_sol = Solveur(N,l,a,v0,dt,eta,Nt) #Calcul des solutions
+###### Calcul de la solution spacio-temporelle #######
+#Calcul de la solution
+if Animation_infini == False :
+    start_time = time.time()
+    x_sol , y_sol, theta_sol = Solveur(N,l,a,v0,dt,eta,Nt) #Calcul des solutions
+    end_time = time.time()
+    print(f"Le temps calcul de la solution est {round(end_time-start_time,2)} secondes.")
 
 #Animation pour vérifier le déplacement des particules
-Animation = False
-Save = False
-Trajectoires = False
-Background = False
-if Animation==True:
+if Animation==True:        
+    start_time = time.time()
     fig, ax = plt.subplots()
     if Background == True : 
         # Charger l'image
         img = mpimg.imread('ciel.jpg')
         # Afficher l'image
         ax.imshow(img, extent=[0, l, 0, l])
-    scat = ax.scatter(x_sol[0], y_sol[0],color='red',zorder=2)
-    # Créer une ligne pour chaque particule
-    lines = [ax.plot([x], [y], color='grey', linewidth=2, zorder=1)[0] for x, y in zip(x_sol[0], y_sol[0])]
 
     # Fixer l'échelle de l'axe
     ax.set_xlim([0, l])
     ax.set_ylim([0, l])
-    
-    def animate(i):
-        scat.set_offsets(np.c_[x_sol[i], y_sol[i]])
-        
-        if Trajectoires==True:
-            # Mettre à jour les données de chaque ligne
-            for j in range(N):
-                xdata, ydata = lines[j].get_data()
-                xdata = np.append(xdata, x_sol[i][j])
-                ydata = np.append(ydata, y_sol[i][j])
-                lines[j].set_data(xdata, ydata)
+
+    if Animation_infini == True :
+        x_t, y_t, theta_t = position_direction_init(N,l)
+        scat = ax.scatter(x_t, y_t,marker='o',color='red',zorder=2,s=100)
+        lines = [ax.plot([x], [y], color='grey', linewidth=2, zorder=1)[0] for x, y in zip(x_t, y_t)]
+        def animate(i):
+            global x_t,y_t,theta_t
+            x_t_old, y_t_old = x_t.copy(), y_t.copy()  # Sauvegarder les positions précédentes
+            x_t,y_t,theta_t = update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t)
+            scat.set_offsets(np.c_[x_t, y_t])
+            
+            if Trajectoires==True:
+                # Mettre à jour les données de chaque ligne
+                for j in range(N):
+                    xdata, ydata = lines[j].get_data()
+                    # Vérifier si la particule a traversé la boîte
+                    if np.abs(x_t[j] - x_t_old[j]) < l / 2 and np.abs(y_t[j] - y_t_old[j]) < l / 2:
+                        xdata = np.append(xdata, x_t[j])
+                        ydata = np.append(ydata, y_t[j])
+                        lines[j].set_data(xdata, ydata)
+    else :
+        scat = ax.scatter(x_sol[0], y_sol[0],marker='h',color='red',zorder=2,s=200)
+        # Créer une ligne pour chaque particule
+        lines = [ax.plot([x], [y], color='grey', linewidth=2, zorder=1)[0] for x, y in zip(x_sol[0], y_sol[0])]
+
+        def animate(i):
+            scat.set_offsets(np.c_[x_sol[i], y_sol[i]])
+            
+            if Trajectoires==True:
+                # Mettre à jour les données de chaque ligne
+                for j in range(N):
+                    xdata, ydata = lines[j].get_data()
+                    # Vérifier si la particule a traversé la boîte
+                    if i > 0 and np.sqrt((x_sol[i][j] - x_sol[i-1][j]) ** 2 + (y_sol[i][j] - y_sol[i-1][j]) ** 2) < l / 2:
+                        xdata = np.append(xdata, x_sol[i][j])
+                        ydata = np.append(ydata, y_sol[i][j])
+                        lines[j].set_data(xdata, ydata)
             
     ani = animation.FuncAnimation(fig, animate, frames=Nt, interval=50)
     if Save==True : 
         ani.save('animation_'+Simulation_name+'.gif', writer='pillow')
     plt.show()
+    end_time = time.time()
+    print(f"Le temps de mise en place de l'annimation est {round(end_time-start_time,2)} secondes.")
--- routines.py
+++ routines.py
@@ -5,21 +5,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 from cmath import *
+import time
+import itertools
 
 def position_direction_init(N,l):
-    #Fonction qui génère les positions et directions initiales des particules
-    x_init =[] #liste des positions initiales (abscisses)
-    y_init = [] #liste des positions initiales (ordonnées)
-    theta_init = [] #liste des directions initiales
-    for i in range(N):
-        x_init.append(np.random.uniform(0,l))
-        y_init.append(np.random.uniform(0,l))
-        theta_init.append(np.random.uniform(0,2*pi))
-        
-    return np.array(x_init), np.array(y_init), np.array(theta_init)
-
+#Fonction qui génère les positions et directions initiales des particules
+###
+# Entrées :
+###
+# N : nombre de particules , l : longueur de la boite
+###
+# Sortie : x_init, y_init, theta_init : positions et directions initiales vecteurs de taille N
+###
+    x_init = np.random.uniform(0, l, N)
+    y_init = np.random.uniform(0, l, N)
+    theta_init = np.random.uniform(0, 2*np.pi, N)
+    return x_init, y_init, theta_init
+    
 def crea_epsilon(eps_x, eps_y):
-    return np.array([[eps_x,0],[0,eps_y]])
+    return np.diag([eps_x, eps_y])
 
 def distance_rnm(x_n, y_n, x_m, y_m,l):
 # Fonction qui calcule la distance entre les individus n et m
@@ -53,40 +57,56 @@ def update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t):
 # Sortie : x_tfut, y_tfut, theta_tfut : positions et directions à l'instant t+dt vecteurs de taille N
 ###
 
+    # Initialisation des vecteurs
     x_tfut=np.zeros((N,1))
     y_tfut=np.zeros((N,1))
     theta_tfut=np.zeros((N,1))
     
     for i in range(N):
         #Calcul des directions à l'instant t+dt pour la particule i
-        arg = []
+        time_init = time.time()
+        arg = 0 + 0*1j
         for j in range(N): 
-        #on parcourt les particules y compris la i car si on ajoute une phase 
-        # et si pas de particule on garde la même direction + var aléatoire
+            #on parcourt les particules y compris la i car on ajoute une phase 
+            #et si pas de particule on garde la même direction + var aléatoire
             rnm = distance_rnm(x_t[i], y_t[i], x_t[j], y_t[j],l)
             if rnm <= a :   
-                arg.append(cos(theta_t[j]) + sin(theta_t[j])*1j)
-            theta_tfut[i] = phase(sum(arg)) + eta*var_al_unif()              
+                arg = arg + (cos(theta_t[j]) + sin(theta_t[j])*1j)
+        theta_tfut[i] = phase(arg) + eta*var_al_unif()
+        time_end = time.time()
+        print(f"Le temps de calcul de la direction de la particule {i} est {round(time_end-time_init,5)} secondes.")  
         #Calcul des positions à l'instant t+dt pour la particule i
-        
+        time_init = time.time()
         x_tfut[i] = (x_t[i] + v0*dt*unit_vector(theta_tfut[i])[0]) % l
         y_tfut[i] = (y_t[i] + v0*dt*unit_vector(theta_tfut[i])[1]) % l  
+        time_end = time.time()
+        print(f"Le temps de calcul de la position de la particule {i} est {round(time_end-time_init,5)} secondes.")
     return np.array(x_tfut), np.array(y_tfut),np.array(theta_tfut)
 
 def Solveur(N,l,a,v0,dt,eta,Nt):
-    
+# Fonction qui calcule les positions et directions des individus à chaque instant
+###
+# Entrées :
+###
+# N,l,a: nombre d'individus, longueur de la boite, échelle de longueur
+# v0,dt,eta,Nt :vitesse, pas de temps, paramètre de bruit, nombre d'itérations
+###
+# Sortie : x_sol, y_sol, theta_sol : positions et directions à chaque instant matrice de taille Nt*N
+
     x_t, y_t, theta_t = position_direction_init(N,l) #initialisation
     x_sol = x_t.reshape(1, -1)
     y_sol = y_t.reshape(1, -1)
     theta_sol = theta_t.reshape(1, -1)
 
     for i in range(Nt-1):
-        x_t, y_t, theta_t = update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t) 
+        time_init = time.time() 
+        x_t, y_t, theta_t = update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t)
         # Ajout des vecteurs aux matrices
         x_sol = np.vstack((x_sol, x_t.reshape(1, -1)))
         y_sol = np.vstack((y_sol, y_t.reshape(1, -1)))
         theta_sol = np.vstack((theta_sol, theta_t.reshape(1, -1)))
-        
+        time_end = time.time()
+        print(f"Le temps de calcul de la solution à l'itération {i} est {round(time_end-time_init,5)} secondes.")
     return x_sol , y_sol, theta_sol
 
 def var_al_unif():
