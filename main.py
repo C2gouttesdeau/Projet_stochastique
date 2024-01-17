45cb7c78aa6733d23a93ac6e103d8949cbb7b5d4
C2gouttesdeau <tanguy.roussin@gmail.com>
2024-01-17 18:05:05 +0100

    création dossier

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import time
 import warnings
 warnings.filterwarnings("ignore")
 from IPython.display import HTML,display
+import os
 
 ##### Import files #####
 import routines
@@ -36,14 +37,26 @@ for N,eta in zip(N,eta):
         plt.show()
 
     ###### Calcul de la solution spacio-temporelle #######
-    x_sol , y_sol, theta_sol,indice_stop  = routines.Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop) #Calcul des solutions
+    x_sol , y_sol, theta_sol,indice_stop  = routines.Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop,Perio) #Calcul des solutions
     if indice_stop < Nt-1:
         print("Convergence de la solution pour Nt=",indice_stop)
         Nt = indice_stop
         x_sol = x_sol[:Nt,:]
         y_sol = y_sol[:Nt,:]
         theta_sol = theta_sol[:Nt,:]
-    name = ("a="+str(a)+"_N="+str(N)+"_eta="+str(eta)+"_v0="+str(v0)+"_Nt="+str(Nt) )  #Nom de la simulation
+    name = ("a="+str(a)+"_N="+str(N)+"_eta="+str(eta)+"_v0="+str(v0)+"_Nt="+str(Nt)+"Perio_"+str(Perio) )  #Nom de la simulation
+    
+    #### Change de répertoire si sauvegarde ####
+    if Save == True:
+        current_dir = os.getcwd()
+        os.chdir("Data")
+        if os.path.exists(name):
+            shutil.rmtree(name)
+        os.makedirs(name)
+        os.chdir(name)
+        print("Sauvegarde dans le dossier : ",name)
+    
+    ###### Sauvegarde des données #######
     if Save == True:
         np.savetxt('x_sol_'+name+'.csv',x_sol) #ligne =temps collones=indices particules
         np.savetxt('y_sol_'+name+'.csv',y_sol)
@@ -58,7 +71,7 @@ for N,eta in zip(N,eta):
         # Initialisation graphique
         scat = plt.scatter([], [],marker='o',color='red',zorder=2,s=size)
         lines = lines = [plt.plot([], [], lw=2,zorder=1)[0]] #[plt.plot([], [], lw=1,zorder=1)[0] for _ in range(x_sol.shape[1])]
-        ani = routines.AnimationGif(x_sol,y_sol,N,l,Nt,100,fram,fig,scat,lines,name,Trajectoires,Save)
+        ani = routines.AnimationGif(x_sol,y_sol,N,l,Nt,100,fram,fig,scat,lines,name,Trajectoires,Save,Perio)
         if Show_Anim:
             plt.show()
         else:
@@ -80,41 +93,116 @@ for N,eta in zip(N,eta):
             plt.show()
         else:
             plt.close()
-        
-        #Calcul de la variance et affichage
-        var_xt,mean_varxt,tx = routines.Calc_var_vec(x_sol,dt)
-        var_yt,mean_varyt,ty = routines.Calc_var_vec(y_sol,dt)
-        plt.figure()
-        plt.plot(tx,var_xt,label="var_xt")
-        plt.plot(ty,var_yt,label="var_yt")
-        plt.plot(tx,mean_varxt,label="mean_varxt")
-        plt.plot(ty,mean_varyt,label="mean_varyt")
+        # #Calcul de la variance et affichage
+        # var_xt,mean_varxt,tx = routines.Calc_var_vec(x_sol,dt)
+        # var_yt,mean_varyt,ty = routines.Calc_var_vec(y_sol,dt)
+        # plt.figure()
+        # plt.plot(tx,var_xt,label="var_xt")
+        # plt.plot(ty,var_yt,label="var_yt")
+        # plt.plot(tx,mean_varxt,label="mean_varxt")
+        # plt.plot(ty,mean_varyt,label="mean_varyt")
+        # plt.xlim(0)
+        # plt.xlabel("Temps")
+        # plt.ylabel("Variance")
+        # plt.legend()
+        # if Save == True:
+        #     plt.savefig("var_"+name+".png")
+        # if Show_Analyse == True:
+        #     plt.show()
+        # else:
+        #     plt.close()
+        # #Calcul de la moyenne et affichage
+        # mean_xt,mean_mean_xt,tx = routines.Calc_mean_vec(x_sol,dt)
+        # mean_yt,mean_mean_yt,ty = routines.Calc_mean_vec(y_sol,dt)
+        # plt.figure("mean")
+        # plt.plot(tx,mean_xt,label="mean_xt")
+        # plt.plot(ty,mean_yt,label="mean_yt")
+        # plt.plot(tx,mean_mean_xt,label="mean_mean_xt")
+        # plt.plot(ty,mean_mean_yt,label="mean_mean_yt")
+        # plt.xlabel("Temps");plt.ylabel("Moyenne")
+        # plt.xlim(0)
+        # plt.legend()
+        # if Save == True:
+        #     plt.savefig("mean_"+name+".png")
+        # if Show_Analyse == True:
+        #     plt.show()
+        # else:
+        #     plt.close()
+            
+        #Calcul du déplacement et affichage
+        mean_d,t = routines.Calc_deplacement(x_sol,y_sol,dt)
+        plt.figure("Deplacement")
+        plt.plot(t,mean_d)
+        plt.xlabel("Temps");plt.ylabel("Deplacement")
         plt.xlim(0)
-        plt.xlabel("Temps")
-        plt.ylabel("Variance")
-        plt.legend()
         if Save == True:
-            plt.savefig("var_"+name+".png")
+            plt.savefig("Deplacement_"+name+".png")
         if Show_Analyse == True:
             plt.show()
         else:
             plt.close()
-
-        #Calcul de la moyenne et affichage
-        mean_xt,mean_mean_xt,tx = routines.Calc_mean_vec(x_sol,dt)
-        mean_yt,mean_mean_yt,ty = routines.Calc_mean_vec(y_sol,dt)
-        plt.figure("mean")
-        plt.plot(tx,mean_xt,label="mean_xt")
-        plt.plot(ty,mean_yt,label="mean_yt")
-        plt.plot(tx,mean_mean_xt,label="mean_mean_xt")
-        plt.plot(ty,mean_mean_yt,label="mean_mean_yt")
-        plt.xlabel("Temps");plt.ylabel("Moyenne")
+        #Calcul de la vitesse et affichage
+        sum_ux,sum_uy,mean_somme_ux,mean_somme_uy,norm,mean_norm,dir,mean_dir,t = routines.Calc_somme_vec_vitesse(theta_sol,dt)
+        plt.figure("Vitesse résulatante")
+        plt.plot(t,sum_ux,label="sum_ux")
+        plt.plot(t,mean_somme_ux,label="mean_sum_ux")
+        plt.plot(t,sum_uy,label="sum_uy")
+        plt.plot(t,mean_somme_uy,label="mean_sum_uy")
         plt.xlim(0)
+        plt.xlabel("Temps");plt.ylabel("Somme vitesse")
         plt.legend()
         if Save == True:
-            plt.savefig("mean_"+name+".png")
+            plt.savefig("Vitesse_"+name+".png")
         if Show_Analyse == True:
             plt.show()
         else:
             plt.close()
+        #Calcul baricentre et affichage
+        x_bari,y_bari,mean_barycentre_x,mean_barycentre_y,n = routines.Calc_baricentre(x_sol,y_sol)
+        plt.figure("Baricentre")
+        plt.plot(n,x_bari,label="x_bari")
+        plt.plot(n,y_bari,label="y_bari")
+        plt.plot(n,mean_barycentre_x,label="mean_x_bari")
+        plt.plot(n,mean_barycentre_y,label="mean_y_bari")
+        plt.xlabel("Particule N°");plt.ylabel("Baricentre - position_initiale")
+        plt.xlim(0)
+        plt.legend()
+        if Save == True:
+            plt.savefig("Baricentre_"+name+".png")
+        if Show_Analyse == True:
+            plt.show()
+        else:
+            plt.close()
+        #deplacement quadratique moyen
+        deplacement_quad_moyen_x,tx = routines.deplacement_quad_mean(x_sol,dt)
+        deplacement_quad_moyen_y,ty = routines.deplacement_quad_mean(y_sol,dt)
+        plt.figure("Deplacement quadratique moyen")
+        plt.plot(tx[:-1],deplacement_quad_moyen_x[:-1],label="deplacement_quad_moyen_x")
+        plt.plot(ty[:-1],deplacement_quad_moyen_y[:-1],label="deplacement_quad_moyen_y")
+        plt.xlabel("Temps");plt.ylabel("Deplacement quadratique moyen")
+        plt.xlim(0)
+        plt.legend()
+        if Save == True:
+            plt.savefig("Deplacement_quad_moyen_"+name+".png")
+        if Show_Analyse == True:
+            plt.show()
+        else:
+            plt.close()
+        #Coefficient de diffusion
+        D,mean_D,t = routines.Diff_coef(x_sol,dt)
+        plt.figure("Coefficient de diffusion")
+        plt.plot(t,D,label="D")
+        plt.plot(t,mean_D*np.ones(len(t)),label="mean_D")
+        plt.xlabel("Temps");plt.ylabel("Coefficient de diffusion")
+        plt.xlim(0)
+        plt.legend()
+        if Save == True:
+            plt.savefig("Diffusion_"+name+".png")
 
+        if Show_Analyse == True:
+            plt.show()
+        else:
+            plt.close()
+    if Save==True:
+        os.chdir(current_dir)
+    
--- routines.py
+++ routines.py
@@ -27,7 +27,7 @@ def distance_indice_matrix(x_t,y_t,l,N,a):
 #Fonction qui renvoie une matrice de 0 et de 1
 # 1 si les particule (i,j) de la matrices sont à une distance inférieur à a
 # 0 sinon 
-    indice_dist = np.eye(N) #les particules sont à une ditance nul d'elles même 
+    indice_dist = np.zeros((N,N)) #les particules sont à une ditance nul d'elles même 
     L = np.array([l, l])
     for i in range(N):
         X_i= np.array([x_t[i], y_t[i]])
@@ -66,9 +66,9 @@ def distance_indice_matrix_vectorized(x_t, y_t, l, N, a):
     # Les particules sont à une distance nulle d'elles-mêmes
     np.fill_diagonal(indice_dist, True)
 
-    return indice_dist.astype(float)
+    return indice_dist#.astype(float)
 
-def update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t):
+def update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t,Perio):
 # Fonction qui calcule les positions et directions des individus à l'instant
 ###
 # Entrées :
@@ -78,24 +78,31 @@ def update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t):
 ###
 # Sortie : x_tfut, y_tfut, theta_tfut : positions et directions à l'instant t+dt vecteurs de taille N
 ###
-    indice_dist = distance_indice_matrix_vectorized(x_t,y_t,l,N,a)
+    if N==1 or a==0:
+        indice_dist = np.zeros((N,N))
+    else:
+        indice_dist = distance_indice_matrix_vectorized(x_t,y_t,l,N,a)
     # Matrice de 0 et 1 pour chaque particule
     lignes_i,collones_j = np.where(indice_dist==True) 
     arg = indice_dist
     #Crétion de arg une matrice de taille NxN avec les angles de theta_t des particule qui sont à une distance inférieur à a
     arg[lignes_i,collones_j] = theta_t[collones_j]
     #Création d'un vecteur contenant les moyennes sur les lignes de arg (sans les prendre en compte les 0)
-    args = np.ma.masked_equal(arg, 0).mean(axis=1)
+    args = np.ma.masked_equal(arg, False).mean(axis=1)
+    args = np.ma.filled(args,0)
     #Création de theta_tfut un vecteur de taille N avec la moyenne des angles de theta_t des particule qui sont à une distance inférieur à a
     theta_tfut = args + eta*np.random.uniform(-pi,pi,N)
-
     #Calcul des positions à l'instant t+dt pour la particule i avec la condition aux bords
-    x_tfut= (x_t + v0*dt*np.cos(theta_tfut)) % l
-    y_tfut= (y_t + v0*dt*np.sin(theta_tfut)) % l 
+    if Perio == True:
+        x_tfut= (x_t + v0*dt*np.cos(theta_tfut)) % l
+        y_tfut= (y_t + v0*dt*np.sin(theta_tfut)) % l 
+    else:
+        x_tfut= (x_t + v0*dt*np.cos(theta_tfut))
+        y_tfut= (y_t + v0*dt*np.sin(theta_tfut)) 
 
     return x_tfut, y_tfut,theta_tfut
 
-def Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop):
+def Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop,Perio):
 # Fonction qui calcule les positions et directions des individus à chaque instant
 ###
 # Entrées :
@@ -113,7 +120,7 @@ def Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop):
     indice_stop = Nt-1
 
     for i in tqdm(range(Nt-1)):
-        x_t, y_t, theta_t = update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t)
+        x_t, y_t, theta_t = update_position_direction(N,l,a,v0,dt,eta,x_t,y_t,theta_t,Perio)
         #Ajout des vecteurs aux matrices
         x_sol[i,:] = x_t
         y_sol[i,:] = y_t
@@ -139,6 +146,33 @@ def Solveur(N,l,a,v0,dt,eta,Nt,coef_dir_limit,Auto_stop):
 #             var_f[i]=np.var(np.mean(f[:(i+1),:]-f[0,:],axis=1))
 #     return var_f,t
 
+def Calc_deplacement(x,y,dt):
+    T = np.shape(x)[0]
+    t = np.arange(0,T,dt)
+    d = np.sqrt((x-x[0,:])**2+(y-y[0,:])**2) #distance entre t0 et t
+    mean_d = np.mean(d,axis=1) # moyenne des déplacements pour l'ensemble des particules 
+    return mean_d[:-1],t[:-1]
+
+def Calc_somme_vec_vitesse(theta_sol,dt):
+    t= np.arange(0,np.shape(theta_sol)[0],dt)
+    ux = np.cos(theta_sol)
+    uy = np.sin(theta_sol)
+
+    sum_ux = np.sum(ux,axis=1)
+    sum_uy = np.sum(uy,axis=1)
+    norm = np.sqrt(sum_ux**2+sum_uy**2)
+    dir = np.arctan(sum_uy/sum_ux)
+    mean_somme_ux = np.zeros(len(t))
+    mean_somme_uy = np.zeros(len(t))
+    mean_norm = np.zeros(len(t))
+    mean_dir = np.zeros(len(t))
+    for i in range(len(t)):
+        mean_somme_ux[i] = np.mean(sum_ux[:(i+1)])
+        mean_somme_uy[i] = np.mean(sum_uy[:(i+1)])
+        mean_norm[i] = np.mean(norm[:(i+1)])
+        mean_dir[i] = np.mean(dir[:(i+1)])
+    return sum_ux[:-1],sum_uy[:-1],mean_somme_ux[:-1],mean_somme_uy[:-1],norm[:-1],mean_norm[:-1],dir[:-1],mean_dir[:-1],t[:-1]
+
 def Calc_var_vec(vec,dt):
     T = np.shape(vec)[0]
     t = np.arange(0,T,dt)
@@ -152,11 +186,32 @@ def Calc_var_vec(vec,dt):
         mean_var_vec[i] = np.mean(var_vec[:(i+1)])
     return var_vec[:-1],mean_var_vec[:-1],t[:-1]
 
+def Calc_baricentre(x,y):
+    N = np.shape(x)[1]
+    n = np.arange(0,N)
+    barycentre_x = np.mean(x,axis=0)-x[0,:]
+    barycentre_y = np.mean(y,axis=0)-y[0,:]
+    mean_barycentre_x = np.ones(N)*np.mean(barycentre_x)
+    mean_barycentre_y = np.ones(N)*np.mean(barycentre_y)
+    return barycentre_x,barycentre_y,mean_barycentre_x,mean_barycentre_y,n
+
+def Diff_coef(x,dt):
+    deplacement_quad_mean_x,t = deplacement_quad_mean(x,dt)
+    D = deplacement_quad_mean_x[1:]/(2*t[1:])
+    mean_D =np.mean(D)
+    return D,mean_D,t[1:]
+
+def deplacement_quad_mean(x,dt):
+    t=np.arange(0,np.shape(x)[0],dt)
+    deplacement_quad_moyen = np.zeros(len(t))
+    for i in range(len(t)):
+        deplacement_quad_moyen[i] = np.mean(np.mean((x[:i+1,:]-x[0,:])**2,axis=0))
+    return deplacement_quad_moyen,t
+
 def Calc_mean_vec(vec,dt):
-    dvec = np.diff(vec, axis=0)
-    T = np.shape(dvec)[0]
+    T = np.shape(vec)[0]
     t = np.arange(0,T,dt)
-    mean_vec = np.mean(dvec,axis=1)
+    mean_vec = np.mean(vec-vec[0,:],axis=1)
     # mean_vec = np.mean(vec-np.mean(vec,axis=0),axis=1)
     mean_mean_vec = np.zeros(len(t))
     for i in range(len(t)):
@@ -175,7 +230,7 @@ def Calc_Phi(theta_t,N,dt):
     Mean_phi,t = [np.mean(phi[:(i+1)]) for i in range(len(phi))],np.arange(0,T,dt)
     return phi, Mean_phi,t
 
-def AnimationGif(x_sol,y_sol,N,l,Nt,inter,fram,fig,scat,lines,name,Trajectoires,Save):
+def AnimationGif(x_sol,y_sol,N,l,Nt,inter,fram,fig,scat,lines,name,Trajectoires,Save,Perio):
     #Définition de la fonction d'animation
     def anifunc(i):
     # Fonction animation qui trace les positions des individus à chaque instant et les trajectoire
@@ -199,8 +254,12 @@ def AnimationGif(x_sol,y_sol,N,l,Nt,inter,fram,fig,scat,lines,name,Trajectoires,
                 new_x = np.append(old_x, x)
                 new_y = np.append(old_y, y)
                 line.set_data(new_x, new_y)
-        plt.xlim(0,l)
-        plt.ylim(0,l)
+        if Perio == True:
+            plt.xlim(0,l)
+            plt.ylim(0,l)
+        else:
+            plt.xlim(np.min(x_sol),np.max(x_sol))
+            plt.ylim(np.min(y_sol),np.max(y_sol))
         return lines + [scat]
     def init_func():
         # Effacer seulement les axes qui contiennent les lignes de trajectoire
